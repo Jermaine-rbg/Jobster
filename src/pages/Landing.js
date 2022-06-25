dc6304fa96d94326019f0d0a1c9d58f4679b826a
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-06-25 10:42:51 -0400

    app.js update

---
+++ src/pages/Landing.js
@@ -0,0 +1,13 @@
+import logo from '../assets/images/logo.svg'
+import main2 from '../assets/images/main2.svg'
+
+const Landing = () => {
+    return (
+        <div>
+        <Landing/>
+        </div>
+    )
+        
+    
+}
+export default Landing;
