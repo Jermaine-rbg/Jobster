1052a35b5ebd47315ca5f50b4b667ad433ba5c06
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-06-27 15:57:52 -0400

    routes

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,58 @@
-const Register = () => {
-    return <div>Register</div>
-};
+import { useState, useEffect } from 'react';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterPage';
+
+const initialState = {
+    name: '',
+    email: '',
+    password: '',
+    isMember: true,
+  };
+
+
+
+
+function Register() {
+    const [values, setValues] = useState(initialState);
+
+    const handleChange = (e) => {
+        console.log(e.target);
+      };
+      
+      const onSubmit = (e) => {
+        e.preventDefault();
+        console.log(e.target);
+      };
+
+
+    return (
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+
+        {/* name field */}
+        <div className='form-row'>
+          <label htmlFor='name' className='form-label'>
+            name
+          </label>
+
+          <input
+            type='text'
+            value={values.name}
+            name='name'
+            onChange={handleChange}
+            className='form-input'
+          />
+        </div>
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
+
+}
 
 export default Register;
