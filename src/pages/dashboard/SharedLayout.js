a9f5803c73d93c0ac2f6267a4bfbf71c0258b6dc
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-06-29 13:15:28 -0400

    complete SharedLayout

--- src/pages/dashboard/SharedLayout.js
+++ src/pages/dashboard/SharedLayout.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Outlet } from "react-router-dom";
+import { BigSidebar, Navbar, SmallSidebar } from "../../components";
+import Wrapper from "../../assets/wrappers/SharedLayout";
+
+
+
+
 
 const SharedLayout = () => {
   return (
-    <h1>SharedLayout</h1>
+    <Wrapper>
+        <main className="dashboard">
+          <SmallSidebar/>
+          <BigSidebar/>
+          <div>
+          <Navbar/>
+          <div className="dashboard-page">
+          <Outlet/>
+          </div>
+          </div>
+        </main>
+    </Wrapper>
   )
 }
 
