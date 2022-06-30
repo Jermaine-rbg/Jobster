74a1b4e332571b7aa41fe1b8b07e103dbc010457
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-06-29 23:15:44 -0400

    navbar complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { FaAlignLeft, FaUserCircle, FaCaretDown } from 'react-icons/fa';
 import Logo from './logo';
 import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { toggleSidebar } from '../features/user/userSlice';
+import { toggleSidebar, logoutUser } from '../features/user/userSlice';
 
 const Navbar = () => {
     const [showLogout, setShowLogout] = useState(false)
@@ -33,7 +33,7 @@ const Navbar = () => {
                 <button 
                 type='button' 
                 className='dropdown-btn' 
-                onClick={()=>console.log('logout user')}>logout</button>
+                onClick={()=>dispatch(logoutUser())}>logout</button>
             </div>
 
         </div>
