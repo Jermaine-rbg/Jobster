83197d19cfb4f23bc336f646517072801783f9a1
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-07-01 17:32:34 -0400

    small side bar setup

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
         }>
           <Route index element={<Stats/>}/>
           <Route path='all-jobs' element={<AllJobs/>}/>
-          <Route path='add-jobs' element={<AddJob/>}/>
+          <Route path='add-job' element={<AddJob/>}/>
           <Route path='profile' element={<Profile/>}/>
            </Route>
         <Route path='landing' element={<Landing />} />
