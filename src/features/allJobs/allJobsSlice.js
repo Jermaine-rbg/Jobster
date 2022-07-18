0e68a2953deb25ce60086a197f0d979b096bfdad
Jermaine-rbg <79242924+Jermaine-rbg@users.noreply.github.com>
2022-07-18 01:58:08 -0400

    change page button

--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -66,6 +66,9 @@ const allJobsSlice = createSlice({
   },
   clearFilters :(state) =>{
     return {...state,...initialFiltersState}
+  },
+  changePage: (state, {payload}) =>{
+    state.page = payload;
   }
   },
   extraReducers: {
@@ -98,5 +101,5 @@ const allJobsSlice = createSlice({
 });
 
 
-export const {showLoading, hideLoading, handleChange, clearFilters} = allJobsSlice.actions;
+export const {showLoading, hideLoading, handleChange, clearFilters, changePage} = allJobsSlice.actions;
 export default allJobsSlice.reducer;
